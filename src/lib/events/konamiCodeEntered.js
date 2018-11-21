4fd58164f0b663b3220bac86d63d80dfcc393c42
Aaron Hardy <aaron@aaronhardy.com>
2018-11-21 06:37:23 -0700

    Swapping double quotes for single quotes.

--- src/lib/events/konamiCodeEntered.js
+++ src/lib/events/konamiCodeEntered.js
@@ -2,9 +2,9 @@
 
 var triggers = [];
 var enteredKeys = [];
-var konami = "38,38,40,40,37,39,37,39,66,65";
+var konami = '38,38,40,40,37,39,37,39,66,65';
 
-window.addEventListener("keydown", function(e) {
+window.addEventListener('keydown', function(e) {
   enteredKeys.push(e.keyCode);
 
   while (enteredKeys.length > 10) {
